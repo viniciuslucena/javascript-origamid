86d1ebd468942f903e9715152b331bed8fd389ce
viniciuslucena <jviniiciuslucena@gmail.com>
2021-04-02 16:55:28 -0300

    02/04

--- Efeitos no DOM/Modal/js/script.js
+++ Efeitos no DOM/Modal/js/script.js
@@ -2,8 +2,12 @@ import initScrollSuave from './modules/scroll-suave.js'
 import initScrollAnimation from './modules/scroll-animation.js'
 import initAccordion from './modules/accordion.js'
 import initTabNav from './modules/tab-nav.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
 
 initScrollSuave()
 initScrollAnimation()
 initAccordion()
 initTabNav()
+initModal()
+initTooltip()
