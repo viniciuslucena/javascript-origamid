8af1960f85154bdf79fc21720d990a3495d07460
viniciuslucena <jviniiciuslucena@gmail.com>
2021-04-06 19:40:38 -0300

    06/04

--- Efeitos no DOM/Modal/js/modules/tooltip.js
+++ Efeitos no DOM/Modal/js/modules/tooltip.js
@@ -6,7 +6,30 @@ export default function initTooltip() {
     })
 
     function onMouseOver(event) {
-        criarTooltipBox(this)
+        const tooltipBox = criarTooltipBox(this)
+
+        onMouseMove.tooltipBox = tooltipBox
+        this.addEventListener('mousemove', onMouseMove)
+
+        onMouseLeave.tooltipBox = tooltipBox
+        onMouseLeave.element = this
+
+        this.addEventListener('mouseleave', onMouseLeave)
+    }
+
+    const onMouseLeave = {
+        handleEvent() {
+            this.tooltipBox.remove()
+            this.element.removeEventListener('mouseleave', onMouseLeave)
+            this.element.removeEventListener('mousemove', onMouseMove)
+        }
+    }
+
+    const onMouseMove = {
+        handleEvent(event) {
+            this.tooltipBox.style.top = event.pageY + 20 + 'px'
+            this.tooltipBox.style.left = event.pageX + 20 + 'px'
+        }
     }
 
     function criarTooltipBox(element) {
